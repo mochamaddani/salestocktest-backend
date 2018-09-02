0c122670fe2c485028dcde9deb93ddf3f41ad4eb
drmochamad <drmochamad7@gmail.com>
2018-09-02 22:43:24 +0700

    fix env mongodb config

--- src/db/dbConfig.js
+++ src/db/dbConfig.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 var dbconfig = function(){
-    mongoose.connect(process.env.DB_URL);
+    mongoose.connect(process.env.MONGODB_URL);
 }
 
 module.exports = dbconfig;
