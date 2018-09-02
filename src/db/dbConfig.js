38c6c613258eed3e4a89e7b049c8fcb018c70ac3
drmochamad <drmochamad7@gmail.com>
2018-09-02 19:26:28 +0700

    add env example

--- src/db/dbConfig.js
+++ src/db/dbConfig.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 var dbconfig = function(){
-    mongoose.connect(process.env.DB_CONFIG);
+    mongoose.connect(process.env.DB_URL);
 }
 
 module.exports = dbconfig;
